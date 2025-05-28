cec9f2808be8db3403b392b5e589f114d8279b97
Wira Atmaja <atmajawira1908@gmail.com>
2025-05-28 11:51:32 +0800

    latest

--- prisma/migrations/20250528033723_add_column_endtime/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `endTime` to the `Schedule` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `schedule` ADD COLUMN `endTime` DATETIME(3) NOT NULL;
